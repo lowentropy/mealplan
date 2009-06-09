2258c28f20860e8eaf6dfa589eebefe574086c04
Nathan Matthews <lowentropy@gmail.com>
2009-06-09 16:06:09 -0500

    some of the migrations are in place; need to import unit/dimension system from tracker, too.

---
+++ db/migrate/20090609205946_create_usda_foods.rb
@@ -0,0 +1,24 @@
+class CreateUsdaFoods < ActiveRecord::Migration
+  def self.up
+  	create_table :usda_foods do |t|
+		t.integer :usda_food_group_id
+		t.text :long_desc
+		t.string :short_desc
+		t.string :common_name
+		t.string :manufacturer_name
+		t.boolean :survey
+		t.string :refuse_desc
+		t.integer :refuse_percent
+		t.string :scientific_name
+		t.float :nitrogen_factor
+		t.float :protein_factor
+		t.float :fat_factor
+		t.float :carb_factor
+		t.timestamps
+	end
+  end
+
+  def self.down
+  	drop_table :usda_foods
+  end
+end
---
+++ db/migrate/20090609210144_create_usda_food_groups.rb
@@ -0,0 +1,12 @@
+class CreateUsdaFoodGroups < ActiveRecord::Migration
+  def self.up
+  	create_table :usda_food_groups do |t|
+		t.string :desc
+		t.timestamps
+	end
+  end
+
+  def self.down
+  	drop-table :usda_food_groups
+  end
+end
